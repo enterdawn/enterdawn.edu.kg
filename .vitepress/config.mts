cdf1f009cff44eb496e09ad06f586a2f3875b0bb
enterdawn <g760420431@outlook.com>
2025-12-15 21:21:21 +0800

    create

---
+++ .vitepress/config.mts
@@ -0,0 +1,27 @@
+import { defineConfig } from 'vitepress'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+    title: "Enterdawn University",
+    description: "Official website of Enterdawn University",
+    themeConfig: {
+        logo: 'logo.png',
+        footer: {
+            message: 'Enterdawn Universuty并不是一所真正的大学，详情请阅读<a style="color: pink;" href="/article/others/about.html">关于本站</a>',
+            copyright: 'Enterdawn university is not a real university. For more details, please read <a style="color: pink;"  href="/article/others/about.html">about us</a>'
+        }
+    },
+    vite: {
+        plugins: [
+            // ...
+            AutoImport({
+                resolvers: [ElementPlusResolver()],
+            }),
+            Components({
+                resolvers: [ElementPlusResolver()],
+            }),
+        ],
+    }
+})
---
+++ .vitepress/theme/Layout.vue
@@ -0,0 +1,71 @@
+<script setup lang="ts">
+import { useData } from 'vitepress'
+
+// https://vitepress.dev/reference/runtime-api#usedata
+const { site, frontmatter } = useData()
+</script>
+<script lang="ts">
+import { h } from 'vue';
+import { ElLink,ElMessage, ElMessageBox } from 'element-plus'
+import type { Action } from 'element-plus'
+const open = () => {
+    const linkNode = h(
+        ElLink,
+        {
+            href: 'https://enterdawn.top' ,
+            target: '_blank',
+            type: 'primary', // 可以使用 ElLink 的所有 props
+            underline: true, // 是否显示下划线
+        },
+        'enterdawn的主页'
+    );
+    ElMessageBox.alert(linkNode, '友情链接', {
+    // if you want to disable its autofocus
+    // autofocus: false,
+        dangerouslyUseHTMLString: true,
+        confirmButtonText: '确定',
+    })
+
+}
+</script>
+
+
+<template>
+    <div class="head">
+        <a href="/"><img style="width: 50px" src="/logo.png"><h1>{{ site.title }}</h1></a>
+        <p>{{ site.description }}</p>
+    </div>
+  <div class="middle-homepage" v-if="frontmatter.home">
+    <ul>
+        <li>
+            <a href="/markdown-examples.html">
+                <h2>示例页面1</h2>
+                <p>示例页面1示例页面1示例页面1示例页面1</p>
+            </a>
+        </li>
+        <li>
+            <a href="/api-examples.html">
+                <h2>示例页面2</h2>
+                <p>示例页面2示例页面2示例页面2示例页面2</p>
+            </a>
+        </li>
+        <li>
+            <a href="/api-examples.html">
+                <h2>示例页面3</h2>
+                <p>示例页面3示例页面3示例页面3示例页面3</p>
+            </a>
+        </li>
+
+    </ul>
+  </div>
+  <div class="middle-otherpage" v-else>
+    <Content />
+  </div>
+   <div class="footer">
+      <div v-html="site.themeConfig.footer.message"></div>
+      <div v-html="site.themeConfig.footer.copyright"></div>
+      <div @click="open">友情链接：点击查看</div>
+      <div>Designed by <a href="https://enterdawn.top/article/others/about.html">enterdawn</a></div>
+      <div>&copy;2026 enterdawn.edu.kg</div>
+  </div>
+</template>
